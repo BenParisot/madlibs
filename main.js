b9601afe761b2c8360e676d1132e4f26aa22c920
Ben Parisot <me@benparisot.com>
2019-01-17 21:09:09 -0800

    finish app

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function story() {
     var bodyPart = document.getElementById('body-part').value;
     var verb = document.getElementById('verb').value;
     
-    var story1 = 'Little ' + adj + ' ' + clothing + ' wanted to go to her Grandmother\’s house. To get there, she had to walk through the scary ' + location + '. On the way there, a Big Bad ' + animal + ' tried to catch her, but she got away. So the Big Bad ' + animal + 'went to Grandmother’s house instead, ate her, put on her clothes, and got into her ' + noun + '.';
+    var story1 = 'Little ' + adj + ' ' + clothing + ' wanted to go to her Grandmother\’s house. To get there, she had to walk through the scary ' + location + '. On the way there, a Big Bad ' + animal + ' tried to catch her, but she got away. So the Big Bad ' + animal + ' went to Grandmother’s house instead, ate her, put on her clothes, and got into her ' + noun + '.';
     
-    var story2 = '\'My Grandmother, what a big ' + bodyPart + ' you have!\' Little ' + clothing +  ' said. She figured out Grandmother was actually the wolf and ' + verb + 'ed him. She then mourned the loss of her beloved Grandmother, went home, and spent years in therapy.';
+    var story2 = '\'My Grandmother, what a big ' + bodyPart + ' you have!\' Little ' + adj + clothing +  ' said. She figured out Grandmother was actually the wolf and ' + verb + 'ed him. She then mourned the loss of her beloved Grandmother, went home, and spent years in therapy.';
     
     var storyTeller1 = document.getElementById('story1');
     var storyTeller2 = document.getElementById('story2');    
