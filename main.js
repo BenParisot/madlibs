c14e781b3ccdbd791a345a937de6dff39da65cd7
Ben Parisot <me@benparisot.com>
2019-01-21 14:10:55 -0800

    clean up main.js

--- main.js
+++ main.js
@@ -8,24 +8,12 @@ function story() {
     var verb = '<span class="word-style">' + document.getElementById('verb').value + 'ed</span> ';
     
     var story1 = 'Little ' + adj + ' ' + clothing + ' wanted to go to her Grandmother\’s house. To get there, she had to walk through the scary ' + location + '. On the way there, a Big Bad ' + animal + ' tried to catch her, but she got away. So the Big Bad ' + animal + ' went to Grandmother’s house instead, ate her, put on her clothes, and got into her ' + noun + '.';
-
-
     
-    var story2 = '\'My Grandmother, what a big ' + bodyPart + ' you have!\' Little ' + adj + clothing +  ' said. She figured out Grandmother was actually the wolf and ' + verb + ' him. She then mourned the loss of her beloved Grandmother, went home, and spent years in therapy.';
+    var story2 = '\'My Grandmother, what a big ' + bodyPart + ' you have!\' Little ' + adj + clothing +  ' said. She figured out Grandmother was actually the wolf so she ' + verb + ' him and ran home, where she mourned the loss of her beloved Grandmother and spent years in therapy.';
     
     var storyTeller1 = document.getElementById('story1');
     var storyTeller2 = document.getElementById('story2');    
     
-    storyTeller1.innerHTML = 'Little ' + adj + clothing + ' wanted to go to her Grandmother\’s house. To get there, she had to walk through the scary ' + location + '. On the way there, a Big Bad ' + animal + ' tried to catch her, but she got away. So the Big Bad ' + animal + ' went to Grandmother’s house instead, ate her, put on her clothes, and got into her ' + noun + '.';
-
-    storyTeller2.innerHTML = '\'My Grandmother, what a big ' + bodyPart + ' you have!\' Little ' + adj + clothing +  ' said. She figured out Grandmother was actually the wolf and ' + verb + ' him. She then mourned the loss of her beloved Grandmother, went home, and spent years in therapy.';
-
-    //Below is the original way 
-    
-    // storyTeller1.textContent = story1;
-    // storyTeller2.textContent = story2; 
-    
-    //the above is taking the content of that id=greeting p element and replacing it with the new variable value
-
-
+    storyTeller1.innerHTML = story1;
+    storyTeller2.innerHTML = story2;
 }
